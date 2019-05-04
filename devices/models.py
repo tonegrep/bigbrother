eafe4c0f9d17f7c43bc0de2aca96dcd0ba61235f
tohue <tonegrep@gmail.com>
2019-05-05 00:48:44 +0300

    controller models updated

--- devices/models.py
+++ devices/models.py
@@ -1,32 +1,57 @@
 from django.db import models
 from django.contrib.auth.models import User
+from model_utils.choices import Choices 
+from model_utils.fields import StatusField
+
+class ControlledDevice(models.Model):
+    name = models.CharField(max_length=30)
+    identifier = models.CharField(max_length=30)
+
+class RemoteControlCode(models.Model):
+    device = models.ForeignKey(ControlledDevice, on_delete=models.CASCADE)
+    function = models.CharField(max_length=20)
+    code = models.CharField(max_length=10)
 
 class System(models.Model):
     name = models.CharField("Smart house", max_length=50)
     address = models.CharField(max_length=50)
     users = models.ManyToManyField(User)
 
-class Device(models.Model):
-    name = models.CharField("Unknown Device", max_length=50)
-    type = models.CharField("General", max_length=50)
+class Controller(models.Model):
+    name = models.CharField("Unknown Controller", max_length=50)
     system = models.ForeignKey(System, on_delete=models.CASCADE)
+    STATUS = Choices('offline', 'ready', 'busy')
+    status = StatusField()
+    class Meta:
+        abstract = True
 
-class Light(Device):
-    pass
-    
+class LightController(Controller):
+    TYPE_CHOICE = Choices('RGB', 'Singlecolor')
+    type = models.CharField(
+        max_length=12,
+        choices=TYPE_CHOICE,
+        default=TYPE_CHOICE.Singlecolor,
+    )
+    brightness = models.PositiveIntegerField()
+
+class RemoteController(Controller):
+    controlled_device = models.ManyToManyField(ControlledDevice)    
+
+class Sensor(Controller):
+    MEASURE_CHOICE = Choices('C', '%')
+    unit_of_measure = models.CharField(
+        max_length=2,
+        choices=MEASURE_CHOICE,
+        default=MEASURE_CHOICE.C,
+    )
 
 class Job(models.Model):
     commands = models.CharField(max_length=200)
     req_date_time = models.DateTimeField(auto_now_add=True)
-    EVERYDAY = 'ED'
-    ONCE = 'ON'
-    RECCURENCE_CHOICE = (
-        (EVERYDAY, 'Everyday'),
-        (ONCE, 'Once'),
-    )
+    RECCURENCE_CHOICE = Choices('Everyday', 'Once')
     reccurence_type = models.CharField(
-        max_length=2,
+        max_length=9,
         choices=RECCURENCE_CHOICE,
-        default=ONCE,
+        default=RECCURENCE_CHOICE.Once,
     )
     reccurence_time = models.TimeField()
