c981ed73cb13eaac78e773d4e1e1c8f902f61ab1
cherenkov <cherenkov@teconpc.local>
2019-05-06 14:48:11 +0300

    admin models registered

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import System, LightController, RemoteController, Sensor
+
+admin.site.register(System)
+admin.site.register(LightController)
+admin.site.register(RemoteController)
+admin.site.register(Sensor)
 
 # Register your models here.
