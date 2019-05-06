3fc53f22fe5a1536e5d35d790ccaf36583f99608
cherenkov <cherenkov@teconpc.local>
2019-05-06 14:49:02 +0300

    cards generation from db

--- devices/views.py
+++ devices/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from devices.models import *
 
 class IndexView(TemplateView):
-    template_name="devices.html"
+    template_name = 'devices.html'
+    def get(self, request, *args, **kwargs):
+        light_items = LightController.objects.all()
+        rc_items = RemoteController.objects.all()
+        sensor_items = Sensor.objects.all()
+        context = {
+        'light_items': light_items,
+        'rc_items' : rc_items,
+        'sensor_items' : sensor_items,
+        }
+        return render(request, 'devices.html', context)
 
 # Create your views here.
