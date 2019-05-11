7576b53440e3e090f5d3394b18af17d60e373b3b
tohue <tonegrep@gmail.com>
2019-05-11 16:53:02 +0300

    working get api with auth check for most of

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,25 @@
-#from .views import
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.conf.urls import include, url
+from django.contrib import admin
+from .views import *
 from rest_framework.authtoken import views as drf_views
-from .views import sample_api
 
 urlpatterns = [
-    path('token-auth/', drf_views.obtain_auth_token),
-    path('devices/', sample_api)
+  path('token-auth/', drf_views.obtain_auth_token),
+#   re_path(r'^controlleddevice/(?P<id>[0-9]+)$', ControlledDeviceAPIView.as_view()),
+#   re_path(r'^controlleddevice/$', ControlledDeviceAPIListView.as_view()),
+#   re_path(r'^remotecontrolcode/(?P<id>[0-9]+)$', RemoteControlCodeAPIView.as_view()),
+#   re_path(r'^remotecontrolcode/$', RemoteControlCodeAPIListView.as_view()),
+  re_path(r'^system/(?P<id>[0-9]+)$', SystemAPIView.as_view()),
+  re_path(r'^system/$', SystemAPIListView.as_view()),
+  re_path(r'^room/(?P<id>[0-9]+)$', RoomAPIView.as_view()),
+  re_path(r'^room/$', RoomAPIListView.as_view()),
+  re_path(r'^lightcontroller/(?P<id>[0-9]+)$', LightControllerAPIView.as_view()),
+  re_path(r'^lightcontroller/$', LightControllerAPIListView.as_view()),
+  re_path(r'^remotecontroller/(?P<id>[0-9]+)$', RemoteControllerAPIView.as_view()),
+  re_path(r'^remotecontroller/$', RemoteControllerAPIListView.as_view()),
+  re_path(r'^sensor/(?P<id>[0-9]+)$', SensorAPIView.as_view()),
+  re_path(r'^sensor/$', SensorAPIListView.as_view()),
+#   re_path(r'^job/(?P<id>[0-9]+)$', JobAPIView.as_view()),
+#   re_path(r'^job/$', JobAPIListView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,42 +1,150 @@
-# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-# from rest_framework.views import APIView
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK
-)
+from rest_framework.views import APIView
+from .serializers import ControlledDeviceSerializer, RemoteControlCodeSerializer, SystemSerializer, RoomSerializer, LightControllerSerializer, RemoteControllerSerializer, SensorSerializer, JobSerializer
+from devices.models import ControlledDevice, RemoteControlCode, System, Room, LightController, RemoteController, Sensor, Job
+from rest_framework.decorators import authentication_classes
+from rest_framework.authentication import TokenAuthentication
 
-# class ExampleView(APIView):
-#     authentication_classes = (SessionAuthentication, BasicAuthentication)
-#     permission_classes = (IsAuthenticated,)
-#     def get(self, request, format=None):
-#         content = {
-#             'user': unicode(request.user),  # `django.contrib.auth.User` instance.
-#             'auth': unicode(request.auth),  # None
-#         }
-#         return Response(content)
+@authentication_classes((TokenAuthentication,))
+class ControlledDeviceAPIView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            item = ControlledDevice.objects.get(pk=id)
+            serializer = ControlledDeviceSerializer(item)
+            return Response(serializer.data)
+        except ControlledDevice.DoesNotExist:
+            return Response(status=404)
 
-@csrf_exempt
-@api_view(["GET"])
-def sample_api(request):
-    data = {'sample_data': 123}
-    return Response(data, status=HTTP_200_OK)
+@authentication_classes((TokenAuthentication,))
+class ControlledDeviceAPIListView(APIView):
+    def get(self, request, format=None):
+        items = ControlledDevice.objects.all()
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(items, request)
+        serializer = ControlledDeviceSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
 
-def api_devices(request):
-    light_items = LightController.objects.filter(system__users=request.user)
-    for item in light_items:
-        if item.id is 2: #change this to if item is available
-            controller_response = requests.get('http://' + item.system.ip + ':' + str(item.port) + '/GET')
-            item.brightness = int(controller_response.content)
-    rc_items = RemoteController.objects.filter(system__users=request.user)
-    sensor_items = Sensor.objects.filter(system__users=request.user)
-    context = {
-    'light_items': light_items,
-    'rc_items' : rc_items,
-    'sensor_items' : sensor_items,
-    }
-    return render(request, 'devices.html', context)
+@authentication_classes((TokenAuthentication,))
+class RemoteControlCodeAPIView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            item = RemoteControlCode.objects.get(pk=id)
+            serializer = RemoteControlCodeSerializer(item)
+            return Response(serializer.data)
+        except RemoteControlCode.DoesNotExist:
+            return Response(status=404)
+
+@authentication_classes((TokenAuthentication,))
+class RemoteControlCodeAPIListView(APIView):
+    def get(self, request, format=None):
+        items = RemoteControlCode.objects.all()
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(items, request)
+        serializer = RemoteControlCodeSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+@authentication_classes((TokenAuthentication,))
+class SystemAPIView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            item = System.objects.get(pk=id)
+            if item.users is request.user:
+                serializer = SystemSerializer(item)
+                return Response(serializer.data)
+            return Response(status=401)
+        except System.DoesNotExist:
+            return Response(status=404)
+
+@authentication_classes((TokenAuthentication,))
+class SystemAPIListView(APIView):
+    def get(self, request, format=None):
+        items = System.objects.filter(users=request.user)
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(items, request)
+        serializer = SystemSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+@authentication_classes((TokenAuthentication,))
+class RoomAPIView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            item = Room.objects.get(pk=id)
+            if item.system.users is request.user:
+                serializer = RoomSerializer(item)
+                return Response(serializer.data)
+            return Response(status=401)
+        except Room.DoesNotExist:
+            return Response(status=404)
+
+@authentication_classes((TokenAuthentication,))
+class RoomAPIListView(APIView):
+    def get(self, request, format=None):
+        items = Room.objects.filter(system__users=request.user)
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(items, request)
+        serializer = RoomSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+@authentication_classes((TokenAuthentication,))
+class LightControllerAPIView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            item = LightController.objects.get(pk=id)
+            if item.system.users is request.user:
+                serializer = LightControllerSerializer(item)
+                return Response(serializer.data)
+            return Response(status=401)
+        except LightController.DoesNotExist:
+            return Response(status=404)
+
+@authentication_classes((TokenAuthentication,))
+class LightControllerAPIListView(APIView):
+    def get(self, request, format=None):
+        items = LightController.objects.filter(system__users=request.user)
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(items, request)
+        serializer = LightControllerSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+@authentication_classes((TokenAuthentication,))
+class RemoteControllerAPIView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            item = RemoteController.objects.get(pk=id)
+            if item.system.users is request.user:    
+                serializer = RemoteControllerSerializer(item)
+                return Response(serializer.data)
+            return Response(status=401)
+        except RemoteController.DoesNotExist:
+            return Response(status=404)
+
+@authentication_classes((TokenAuthentication,))
+class RemoteControllerAPIListView(APIView):
+    def get(self, request, format=None):
+        items = RemoteController.objects.filter(system__users=request.user)
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(items, request)
+        serializer = RemoteControllerSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+@authentication_classes((TokenAuthentication,))
+class SensorAPIView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            item = Sensor.objects.get(pk=id)
+            if item.system.users is request.user:
+                serializer = SensorSerializer(item)
+                return Response(serializer.data)
+            return Response(status=401)
+        except Sensor.DoesNotExist:
+            return Response(status=404)
+
+@authentication_classes((TokenAuthentication,))
+class SensorAPIListView(APIView):
+    def get(self, request, format=None):
+        items = Sensor.objects.filter(system__users=request.user)
+        paginator = PageNumberPagination()
+        result_page = paginator.paginate_queryset(items, request)
+        serializer = SensorSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
